5a1e751b39f5bd523648e00602daee91ede08576
Ca Rem Kem <baoanh200510@gmail.com>
2018-06-19 00:22:29 +0700

    [anh.tran] Change Entity Framework

---
+++ XuatNhapKho/BLL/BLL/UserBLL/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BLL.UserBLL
+{
+	public class User : IUserBLL
+	{
+		public IQueryable<User> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
