695ac9d9da2229d02fb4d3dd0b3dda9153a4ba9c
Ca Rem Kem <baoanh200510@gmail.com>
2018-06-19 00:11:24 +0700

    [anh.tran] 2nd commit

---
+++ XuatNhapKho/DAL/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DAL
+{
+	public class Class1
+	{
+		private string _connectionString;
+		public Class1()
+		{
+			if (string.IsNullOrWhiteSpace(_connectionString))
+				_connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+		}
+		public void TestConnection()
+		{
+
+		}
+	}
+}
